08bfd996c3926cc94bdfc4ebe07b47f91c00191e
Mugi0297 <mugi.manoj.97@gmail.com>
2025-03-21 19:49:42 +0530

    initial commit

---
+++ Session-01/Hello1.java
@@ -0,0 +1,30 @@
+class Hello1{
+        public static void main(String[] args){
+	System.out.println("Hello world.."); //Statement
+}
+}
+public-->Access modifier
+static-->keyword
+void--->return type
+main()-->method
+String[] args-->Command Line Argument
+Printstream library
+System-->class
+out-->Object
+println()-->printstream method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
